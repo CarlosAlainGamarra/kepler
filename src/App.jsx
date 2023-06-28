0537a48f27787512f78dfbd7c7538b1b89160e64
Carlos Gamarra <carlosalaingamarra@gmail.com>
2023-06-28 08:22:58 -0300

    Creando de estructura inicial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Footer } from "./components/footer/Footer"
+import { NavBar } from "./components/navbar/NavBar"
+import { Slider } from "./components/slider/Slider"
+
+function App() {
+
+  return (
+    <>
+      <NavBar />
+      <Slider />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,15 @@
+//import { Link } from "react-router-dom"
+import { Menu } from "../menu/Menu"
+
+export const NavBar = () => {
+  return (
+    <nav>
+      <div>logo</div>
+      <div>Item 1</div>
+      <div>Item 2</div>
+      <div>Item 3</div>
+      <Menu />
+    </nav>
+  )
+}
+
